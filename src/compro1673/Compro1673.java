7d88a43bef885205e1ef3d9a584eb1d1a309c2ea
beings <downn.Syndrome@hotmail.com>
2012-06-19 15:48:51 +0700

    first commit

---
+++ src/compro1673/Compro1673.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compro1673;
+
+/**
+ *
+ * @author thanareepoodlek
+ */
+public class Compro1673 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
