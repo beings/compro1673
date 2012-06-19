31a1bb12a885705a4e74fbf0fec0cc19b25b602d
beings <downn.Syndrome@hotmail.com>
2012-06-19 16:04:29 +0700

    สร้าง HelloWorld

--- src/th/ac/nu/students/u54341673/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/students/u54341673/compro/ch01/HelloWorld.java
@@ -2,18 +2,18 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package compro1673;
+package th.ac.nu.students.u54341673.compro.ch01;
 
 /**
  *
  * @author thanareepoodlek
  */
-public class Compro1673 {
+public class HelloWorld {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("HelloWorld");
     }
 }
